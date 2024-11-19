20ea7e5b0a7de23bd92f49af7a9ab365af876568
angeljohanj <angeljohanjimenezcampo@gmail.com>
2024-11-19 01:00:27 -0400

    changes made in program.cs

--- Program.cs
+++ Program.cs
@@ -2,28 +2,20 @@ using PuntoDeVenta_API.DependencyInjections;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container by going to AddInjections method.
 
 builder.Services.AddInjections();
 
 var app = builder.Build();
 
-//Enable CORS
-
-app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
+    app.UseSwagger()
+        .UseSwaggerUI()
+        .UseHttpsRedirection()
+        .UseAuthentication()
+        .UseAuthorization()
+        .UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 app.MapControllers();
 
